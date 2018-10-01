2b050f477146bf4070f85362b84654dd1ebe20a9
root-account <kbmashiloane@outlook.com>
2018-10-01 14:10:18 +0200

    Some few edits

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import AppForms from './components/AppForms';
+import AppTbl from './components/AppTbl';
+import AppBadges from './components/AppBadges';
+import AppButtons from './components/AppButtons';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<AppForms />, document.getElementById('appforms'));
+ReactDOM.render(<AppTbl />, document.getElementById('appTbl'));
+ReactDOM.render(<AppBadges />, document.getElementById('appBadges'));
+ReactDOM.render(<AppButtons />, document.getElementById('appBtns'));
+
